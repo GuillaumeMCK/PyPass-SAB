545cd9944d8a12b3923d840fcabd27c7bce050c2
GuillaumeMCK <guillaume.mucwicki@gmail.com>
2025-03-12 16:11:14 +0100

    chore: remove gen_patch

--- gen_patch.py
+++
@@ -1,7 +0,0 @@
-from src.patcher import Patcher
-
-Patcher.gen_patches(
-    'C:/Program Files/StartAllBack/StartAllBackX64 - Copie.dll',
-    'C:/Program Files/StartAllBack/StartAllBackX64.dll',
-    'v3.6.5'
-)
